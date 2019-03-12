54f44b8679458e960bc56d82eda4a6953df5a831
MichaelKatos <katoskreations@gmail.com>
2019-03-12 17:35:57 -0400

    Continuing Validation

--- validation/register.js
+++ validation/register.js
@@ -4,6 +4,8 @@ const isEmpty = require('./is-empty');
 module.exports = function validateRegisterInput(data) {
   let errors = {};
 
+  data.name = !isEmpty(data.name) ? data.name : '';
+
   //Setting name validator to be between 2 and 30 characters
   if (!validator.isLength(data.name, {
       min: 2,
@@ -12,8 +14,12 @@ module.exports = function validateRegisterInput(data) {
     errors.name = 'Name must be between 2 and 30 characters';
   }
 
+  if (validator.isEmpty(data.name)) {
+    errors.name = 'Name field is required';
+  }
+
   return {
     errors,
     isValid: isEmpty(errors)
-  }
-}
+  };
+};
